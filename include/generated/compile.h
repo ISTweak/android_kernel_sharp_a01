012538924303b6825614856281d738aa15001411
ISTweak <istweak@d-o-b.org>
2013-01-24 10:37:08 +0900

    cleaning

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 5 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 PREEMPT Thu Jan 24 09:06:21 JST 2013"
-#define LINUX_COMPILE_TIME "09:06:21"
-#define LINUX_COMPILE_BY "love"
-#define LINUX_COMPILE_HOST "marijuana"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.6.3 (Sourcery CodeBench Lite 2012.03-56) "
